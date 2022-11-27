3f24624863abb1db0e8bb10595b64e81a220dfc1
tkrupesh14 <info.tkrupesh@gmail.com>
2022-11-27 17:33:03 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import { Heading } from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Heading color='tomato'> Welcome to Codedu Community Portal</Heading>
+   <Button colorScheme='teal' variant='outline'>
+    Coming Soon! 🌟
+  </Button>
     </div>
   );
 }
